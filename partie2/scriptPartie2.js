a52133cbe3c34446696cc3781201eb708136002e
Morgan Boreatti <m.boreatti@gmail.com>
2017-01-22 23:57:17 +0100

    Add files via upload

--- partie2/scriptPartie2.js
+++ partie2/scriptPartie2.js
@@ -4,7 +4,7 @@
 
 var p = 0;
 $.arrPreload = [];
-$.arrPreload[p++] = "img/bgMenuPartie2.jpg";
+$.arrPreload[p++] = "img/bgPartie2-3.jpg";
 
 
 $(document.createElement('img')).bind('load', function () {
@@ -20,3 +20,44 @@ function preloadDone(){
   }, 300);
 
 };
+
+
+
+
+
+$(document).ready(function() {
+
+
+/********************************************************   
+----- - MENU PLIE/DEPLIE -   ---- 
+******************************************************/
+ $(function(){
+        $('.btnRepli').on('click touch', function(e){
+            $('.menuChapitres').slideToggle();
+            // $('.btnRepli p').replaceWith('<p>Montrer le menu</p>');
+            e.preventDefault();
+
+            if ($('.btnRepli p').html() == 'Replier le menu'){
+       			 $('.btnRepli p').replaceWith('<p>Montrer le menu</p>');
+    		}
+    		else{
+    			$('.btnRepli p').replaceWith('<p>Replier le menu</p>');
+    		}
+
+          });
+
+        });
+
+});
+
+// function afficherMasquer() {  
+//     if (document.getElementById("pRepli").innerHTML == "Replier le menu")
+//     {
+//     	// alert("ok");
+//         document.getElementById("pRepli").innerHTML ="Montrer le menu";  
+//     }
+//     else
+//     {
+//         document.getElementById("pRepli").innerHTML ="Replier le menu";  
+//     }
+// };
