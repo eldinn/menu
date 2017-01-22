5f448a4482f1422c6c4619305785e98b037b75ca
Morgan Boreatti <m.boreatti@gmail.com>
2017-01-22 23:58:06 +0100

    Add files via upload

---
+++ partie3/scriptPartie3.js
@@ -0,0 +1,48 @@
+/********************************************************   
+----- - PRELOAD -   ---- 
+******************************************************/
+
+var p = 0;
+$.arrPreload = [];
+$.arrPreload[p++] = "img/bgPartie3.jpg";
+
+
+$(document.createElement('img')).bind('load', function () {
+  if ($.arrPreload.length > 0)
+    this.src = $.arrPreload.shift()
+  else
+    preloadDone();
+}).trigger('load');
+
+function preloadDone(){
+  setTimeout(function () {
+    $("#oLoading").html("").fadeOut();
+  }, 300);
+
+};
+
+
+$(document).ready(function() {
+
+
+/********************************************************   
+----- - MENU PLIE/DEPLIE -   ---- 
+******************************************************/
+ $(function(){
+        $('.btnRepli').on('click touch', function(e){
+            $('.menuChapitres').slideToggle();
+            // $('.btnRepli p').replaceWith('<p>Montrer le menu</p>');
+            e.preventDefault();
+
+            if ($('.btnRepli p').html() == 'Replier le menu'){
+       			 $('.btnRepli p').replaceWith('<p>Montrer le menu</p>');
+    		}
+    		else{
+    			$('.btnRepli p').replaceWith('<p>Replier le menu</p>');
+    		}
+
+          });
+
+        });
+
+});
